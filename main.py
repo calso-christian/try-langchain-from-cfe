42c1cf2cf15a8dda1820d1fe1b48f03d0553c086
calso-christian <calso.cpr@gmail.com>
2025-03-21 14:05:57 +0800

    Added simple REST API with FastAPI

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def home_page():
+    return {"hello":"world"}
+
+if __name__ == "__main__":
+    
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=8100)
+
+
